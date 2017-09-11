9cb6527dcd65a0e918cd89819039098ccd3474be
Alex Kopen <alexkopen@gmail.com>
2017-09-11 16:18:21 -0500

    some application work

--- src/app/dashboard/applications/single-application/single-application.component.ts
+++ src/app/dashboard/applications/single-application/single-application.component.ts
@@ -7,31 +7,11 @@ import {Application} from '../../../shared/models/application.model';
   styleUrls: ['./single-application.component.scss']
 })
 export class SingleApplicationComponent implements OnInit {
-  years = new Array<number>();
-  days = new Array<number>();
   @Input() currentApplication: Application;
 
   constructor() { }
 
   ngOnInit() {
-    this.generateYears();
-    this.generateDays();
-  }
-
-  private generateYears() {
-    const currentYear = (new Date()).getFullYear();
-
-    for (let i = currentYear; i >= currentYear - 100; i--) {
-      this.years.push(i);
-    }
-  }
-
-  private generateDays() {
-    const currentDay = 31;
-
-    for (let i = currentDay; i >= 1; i--) {
-      this.days.push(i);
-    }
   }
 
 }
