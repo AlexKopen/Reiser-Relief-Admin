353aee2c4de3a4b25012b0bbd05b0381a5122b42
Alex Kopen <alexkopen@gmail.com>
2018-04-03 12:39:59 -0500

    Modify trip full functionality

--- src/app/modify-trip-view/modify-trip-view.component.ts
+++ src/app/modify-trip-view/modify-trip-view.component.ts
@@ -1,7 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { TripDate } from '../shared/models/trip-date.model';
 import { DataService } from '../shared/data.service';
-import Swal from "sweetalert2";
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-modify-trip-view',
@@ -14,16 +14,25 @@ export class ModifyTripViewComponent implements OnInit {
   @Output() updateTripDate = new EventEmitter();
 
   tripLeader: string;
+  tripDate: string;
+  tripStatus: string;
+  @ViewChild('dateInput') dateInput;
+
+  loading = false;
 
   constructor(private dataService: DataService) {
   }
 
   ngOnInit() {
     this.tripLeader = this.selectedTripDate.tripLeader;
+    this.tripDate = this.selectedTripDate.date;
+
+    const flatPicker = require('flatpickr');
+    flatPicker(this.dateInput.nativeElement, {'defaultDate': this.tripDate});
   }
 
   get updateDisabled(): boolean {
-    return this.tripLeader.length === 0;
+    return this.tripLeader.length === 0 || this.tripDate.length === 0;
   }
 
   closeClick(): void {
@@ -32,7 +41,30 @@ export class ModifyTripViewComponent implements OnInit {
 
   updateClick(): void {
     this.selectedTripDate.tripLeader = this.tripLeader;
+    this.selectedTripDate.date = this.tripDate;
+    this.selectedTripDate.status = this.tripStatus;
+
+    this.loading = true;
     this.dataService.updateTripDate(this.selectedTripDate).subscribe(data => this.sendUpdate());
+  }
+
+  changeTripStatusClick(): void {
+    switch (this.selectedTripDate.status) {
+      case 'Open':
+        this.tripStatus = 'Closed';
+        break;
+      case 'Closed':
+        this.tripStatus = 'Open';
+        break;
+      default:
+        break;
+    }
+
+    this.selectedTripDate.status = this.tripStatus;
+  }
+
+  get oppositeTripStatus(): string {
+    return this.selectedTripDate.status === 'Open' ? 'Closed' : 'Open';
   }
 
   private sendUpdate(): void {
